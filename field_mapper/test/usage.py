9eb64ccaf2d5bd43c8fdafa1f4a903f6689af164
Palash <ariful.islamk777@gmail.com>
2024-12-07 05:37:16 +0600

    updated usage.py

--- field_mapper/test/usage.py
+++ field_mapper/test/usage.py
@@ -7,24 +7,26 @@ def validate_email(value: str) -> bool:
 
 
 fields = {
-    "name": {"type": str, "max_length": 50, "required": True},
-    "email": {"type": str, "max_length": 100, "required": True, "custom": validate_email},
-    "phone": {"type": str, "max_length": 15, "required": False}
+    "name": {"type": str, "max_length": 50, "required_field": True, "required_value":True},
+    "email": {"type": str, "max_length": 100, "required_field": True, "required_value":True, "custom": validate_email},
+    "phone": {"type": str, "max_length": 15, "required_field": False, "required_value":False},
+    "income": {"type": int, "max_length": 15, "required_field": True, "required_value":False}
 }
 field_map = {
     "name": "full_name",
     "email": "contact_email",
-    "phone": "mobile_number"
+    "phone": "mobile_number",
+    "income": "monthly_income"
 }
-
-mapper = FieldMapper(fields, field_map)
 
 data = [
     {"name": "Alice", "email": "alice@example.com", "phone": "1234567890"},
-    {"name": "Bob", "email": "invalid-email"},
-    {"name": "Charlie", "email": "charlie@example.com"}
+    {"name": "Bob", "email": "charlieexample.com", "phone": "453543535", "income":0},
+    {"name": "Charlie", "email": "charlie@example.com", "phone": "34534523", "income":0}
 ]
+mapper = FieldMapper(fields, field_map)
 # data_validate = {"name": "Alice", "email": "alice@example.com", "phone": "1234567890"}
 # print(mapper.validate(data_validate))
 processed_data = mapper.process(data)
 print(processed_data)
+print(mapper.error)
